8f60bef63351d8c54cf861aaed46b04cde5558c7
CynthiaLlamas <98438989+CynthiaLlamas@users.noreply.github.com>
2023-02-16 13:50:58 -0700

    base player

---
+++ Othello/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Othello
+{
+    class Player
+    {
+        private String name;
+        public void TakeTurn() {
+            
+        }
+        public String GetName() {
+            return this.name;
+        }
+        public void SetName(String name) {
+            this.name = name;
+        }
+
+    }
+}
