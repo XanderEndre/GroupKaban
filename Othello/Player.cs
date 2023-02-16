9df2799c45509d7a852e365eab006324426fafa8
CynthiaLlamas <98438989+CynthiaLlamas@users.noreply.github.com>
2023-02-16 14:10:29 -0700

    Player Class With Piece

--- Othello/Player.cs
+++ Othello/Player.cs
@@ -9,6 +9,7 @@ namespace Othello
     class Player
     {
         private String name;
+        private Piece colour;
         public void TakeTurn() {
             
         }
@@ -17,6 +18,13 @@ namespace Othello
         }
         public void SetName(String name) {
             this.name = name;
+        }
+        public void SetColour(Piece colour)
+        {
+            this.colour = colour;
+        }
+        public Piece GetColour() { 
+            return new Piece();
         }
 
     }
