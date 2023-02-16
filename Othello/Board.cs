9151b3d72e84e48a855e175a7dab8815d77475c6
Xander Endre <45239432+ShadyAlexCodes@users.noreply.github.com>
2023-02-16 13:56:56 -0700

    Starter methods implemented

--- Othello/Board.cs
+++ Othello/Board.cs
@@ -11,6 +11,10 @@ namespace Othello
     {
         // Create an Array of Titles in an 8x8 array
         public Tile[,] listOfTiles = new Tile[8, 8];
+        
+        public void resetBoard() { }
+
+        public void initializeBoard() { }
 
     }
 }
